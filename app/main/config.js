c985b0a28726dec094a96ffdb8207ca0f4944fe7
h.cai <caihech@hotmail.com>
2018-05-14 16:01:29 +0800

    更新路由格式

--- app/main/config.js
+++ app/main/config.js
@@ -5,22 +5,23 @@
 
     mainModule.config(function ($stateProvider, $urlRouterProvider) {
 
-        console.info("this si config.");
-
         $urlRouterProvider.otherwise('/login');
 
-        $stateProvider.state('home', {
-            url: '/home',
-            templateUrl: 'home/home.html',
-            controller: 'HomeController'
+        $stateProvider
+            .state(
+                'home', {
+                    url: '/home',
+                    templateUrl: 'home/home.html',
+                    controller: 'HomeController'
 
-        }).state(
-            "login",
-            {
-                url: '/login',
-                templateUrl: 'login/login.html',
-                controller: 'LoginController'
-            }
-        );
+                }
+            )
+            .state(
+                "login", {
+                    url: '/login',
+                    templateUrl: 'login/login.html',
+                    controller: 'LoginController'
+                }
+            )
     });
 })();
